784a2bce085cc75d52b3acc1f2cdd1811e199a68
Orville-Mo-He <81935112+Orville-Mo-He@users.noreply.github.com>
2021-11-17 14:29:00 -0500

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,11 @@
+for i in range(36):
+    message = f'<img src="assets\Visual Language A\Set1\img ({i}).jpeg" alt="collage {i}">'
+    # print(message)
+
+for i in range(1, 11):
+    message = f'<img src="assets\Visual Language A\Set2\img ({i}).jpg" alt="Set 2 composition: {i}">'
+    print(message)
+
+for i in range(1, 21):
+    message = f'<img src="assets\Visual Language A\Set3\img ({i}).jpg" alt="Set 3 composition: {i}">'
+    # print(message)
